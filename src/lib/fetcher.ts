01d6c28d47bcac4613fa02a578aed4a8d6948532
yu.dh <yudonghan.com@gmail.com>
2024-02-04 10:31:18 +0800

    demo static data

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 import { BasicInfoRes } from "./fetch";
 import { idLsKey } from "@/app/hooks/useAppConfig";
-
+import basicInfo from "./demo/basicInfo";
+import elements from "./demo/elements";
+import live from "./demo/live";
+import picks from "./demo/picks";
+import history from "./demo/history";
 export interface SolvedBasicInfo extends BasicInfoRes {
   updateAt: string;
 }
 export const userIdfetcher = (key: string) => {
+  if (window.location.search === "?demo") {
+    return "5524951";
+  }
   const lsId = window.localStorage.getItem(idLsKey);
   if (lsId) {
     return lsId;
@@ -13,6 +20,20 @@ export const userIdfetcher = (key: string) => {
   return "";
 };
 export const fetcher = (url: string) => {
+  if (window.location.search === "?demo") {
+    if (url.indexOf("elements") > -1) {
+      return elements;
+    }
+    if (url.indexOf("history") > -1) {
+      return history;
+    }
+    if (url.indexOf("live") > -1) {
+      return live;
+    }
+    if (url.indexOf("picks") > -1) {
+      return picks;
+    }
+  }
   const ls = window.localStorage.getItem(url);
   if (ls) {
     return JSON.parse(ls);
@@ -25,6 +46,10 @@ export const fetcher = (url: string) => {
 };
 
 export const basicInfofetcher = (url: string) => {
+  if (window.location.search === "?demo") {
+    return basicInfo;
+  }
+
   const ls = window.localStorage.getItem(url);
   if (ls) {
     return JSON.parse(ls);
